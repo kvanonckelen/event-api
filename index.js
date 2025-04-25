118361458af216cd7ef8dda3037e4f32b0cc2985
Kevin Van Onckelen <42092016+kevinvobw@users.noreply.github.com>
2025-04-25 09:51:50 +0200

    index update

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({allowOrigin: true, origin: "*"}));
 app.use("/api", authRoutes);
 
-app.use(bodyParser.json());
-
 app.use('/webhook', webhookRouter);
 
 app.listen(PORT, () => {
